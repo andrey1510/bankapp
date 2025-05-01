4505665524bc8551c9856be484d1dfd002c1e94a
andrey1510 <andrey.ivn15@gmail.com>
2025-05-01 23:54:09 +0300

    services created

---
+++ cashservice/src/main/java/com/cashservice/CashserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cashservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashserviceApplication.class, args);
+	}
+
+}
---
+++ cashservice/src/test/java/com/cashservice/CashserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
